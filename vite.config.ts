0efcc5a6da9b161133885328a12e91e693976620
Your Name <you@example.com>
2024-12-27 00:25:32 -0300

    auto

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/voice-avatar-assistant/", // Substitua pelo nome do repositório
+ // base: "/voice-avatar-assistant/", // Substitua pelo nome do repositório
   server: {
     host: "::",
     port: 8080,
